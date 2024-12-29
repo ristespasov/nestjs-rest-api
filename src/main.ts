c4fee46512252d6761b8b1a557c1bed40a42e831
ristespasov <ristespasov@hotmail.com>
2024-12-29 20:40:28 +0100

    exceprions, cors & global prefix setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { AllExceptionsFilter } from './all-exceptions.filter';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+
+  app.enableCors();
+  app.setGlobalPrefix('api');
+  await app.listen(3000);
 }
 bootstrap();
