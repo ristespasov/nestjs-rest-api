29f4a7e867eea4b991bf65daeffc2461b6b378d6
ristespasov <ristespasov@hotmail.com>
2024-12-31 15:52:42 +0100

    method exported

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AllExceptionsFilter } from './all-exceptions.filter';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+export async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const { httpAdapter } = app.get(HttpAdapterHost);
